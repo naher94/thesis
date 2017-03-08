e46ae3c93e3c8b490427a74ec07187d774850ff1
Rehan Butt <rab@Rehans-MacBook-Pro-2.local>
2017-03-08 16:23:39 -0500

    Adding More things

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ## how to run the app
 
 `DEBUG=thesis:* nodemon npm start`  
+
+https://pugjs.org/api/reference.html
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  
   var watson = require('watson-developer-cloud');
   var alchemy_language = watson.alchemy_language({
     api_key: process.env.API_KEY
   })
   var parameters = {
     url: 'https://www.nytimes.com/2017/03/07/technology/uber-travis-kalanick.html?rref=collection%2Fsectioncollection%2Ftechnology&action=click&contentCollection=technology&region=rank&module=package&version=highlights&contentPlacement=1&pgtype=sectionfront'
+    , knowledgeGraph: 1
+    , showSourceText: 1
+    
   };
   alchemy_language.keywords(parameters, function (err, response) {
     if (err) console.log('error:', err);
-    else{ res.render('index', {
-    title: 'Conversational Design',
-    data: JSON.stringify(response, null, 2)
-  });}
+    else {
+      res.render('index', {title: 'Conversational Design', data: JSON.stringify(response, null, 2)});
+    }
 //    else console.log(JSON.stringify(response, null, 2));
   });
-
 });
 module.exports = router;
