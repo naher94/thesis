c989a83caeefaa3d9385683b7702bf154ec73ea0
Rehan Butt <rab@Rehans-MBP-2.wv.cc.cmu.edu>
2017-03-08 02:13:08 -0500

    Environment stuff added

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.get('/', function (req, res, next) {
   
   var watson = require('watson-developer-cloud');
   var alchemy_language = watson.alchemy_language({
-    api_key: 'API_KEY'
+    api_key: process.env.API_KEY
   })
   var parameters = {
     url: 'https://www.nytimes.com/2017/03/07/technology/uber-travis-kalanick.html?rref=collection%2Fsectioncollection%2Ftechnology&action=click&contentCollection=technology&region=rank&module=package&version=highlights&contentPlacement=1&pgtype=sectionfront'
